f5dffb4c261787f5cb8c63de9c466a6b693512bb
bhosley <brandonscotthosley@gmail.com>
2020-08-01 04:04:35 -0600

    Basic Loop Function
    Added basic functionality/outline of functionality to the main loop

--- sketch_stormy_weather/sketch_stormy_weather.ino
+++ sketch_stormy_weather/sketch_stormy_weather.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_NeoPixel.h>
 
+#define LED_PIN 4
+#define NUM_LEDS 2
+
+Adafruit_NeoPixel lightning = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+int strikeDenominator = 10;
+
+void setup() {
+  // Neopixel setup
+  lightning.begin();
+  lightning.show(); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Lightning Chance
+  if (random(strikeDenominator) == 3) {
+    int led = ranodm(NUM_LEDS);
+    for (int i = 0; i < 10; i++) {
+      lightningStrike(random(NUM_LEDS));
+    }
+    /* Increased odds for follow-on strike (decrease denominator) */
+  } else {
+    /* Decrease the odds of strike (Increase denominator) */
+  }
+  /* Turn off all Pixels */
+  /* Delay between Loops */
 }
+
+/* To Black Function */
+/* lightning strike function */
