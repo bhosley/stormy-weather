27f9ae2c33ad39b5f1064fe4a1427b0f45a81ebb
bhosley <brandonscotthosley@gmail.com>
2020-08-01 07:25:23 -0600

    Update sketch_stormy_weather.ino
    Complete rewrite. Borrowed from One-Guy-One-Blog. New basis

--- sketch_stormy_weather/sketch_stormy_weather.ino
+++ sketch_stormy_weather/sketch_stormy_weather.ino
@@ -1,35 +1,82 @@
-#include <Adafruit_NeoPixel.h>
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
 
-#define LED_PIN 4
-#define NUM_LEDS 2
+int ledPin = 9;    // LEDs (via MOSFET) connected to pin 9
+int rxPin = 10;    // DFplayer RX to Arduino pin 10
+int txPin = 11;    // DFplayer TX toArduinopin 11
+int busyPin = 12;  // DFplayer BUSY connected to pin 12
 
-Adafruit_NeoPixel lightning = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
-int strikeDenominator = 10;
+SoftwareSerial mySoftwareSerial(rxPin, txPin);
+DFRobotDFPlayerMini myDFPlayer;
 
-void setup() {
-  // Lightning setup
-  lightning.begin();
-  lightning.show(); 
+void setup()
+{
 
-  // TODO Rain Loop Setup
-}
+  pinMode(ledPin, OUTPUT);
+  pinMode(busyPin, INPUT);
 
-void loop() {
-  // Lightning Chance
-  if (random(strikeDenominator) == 3) {
-    int led = ranodm(NUM_LEDS);
-    for (int i = 0; i < 10; i++) {
-      callLightning(random(NUM_LEDS));
-      // TODO Call thunder interrupt
-    }
-    // TODO Increased odds for follow-on strike (decrease denominator) 
-  } else {
-    // TODO Decrease the odds of strike (Increase denominator)
+  mySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+  Serial.println(F("Initializing DFPlayer..."));
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin. Check connection and SD card, or reset the Arduino."));
+    while (true);
   }
-  // TODO Turn off all Pixels
-  // TODO Delay between Loops
+
+  Serial.println(F("DFPlayer Mini online."));
+
+  myDFPlayer.setTimeOut(500);                   // Set serial communictaion time out 500ms
+  myDFPlayer.volume(30);                        // Set volume value (0~30).
+  myDFPlayer.EQ(DFPLAYER_EQ_BASS);              // Set EQ to BASS (normal/pop/rock/jazz/classic/bass)
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);  // Set device we use SD as default
+  myDFPlayer.enableDAC();                       // Enable On-chip DAC
 }
 
-// To Black Function
-// lightning strike function
-// thunder sound interrupt
+void loop()
+{
+  int flashCount = random (3, 15);        // Min. and max. number of flashes each loop
+  int flashBrightnessMin =  10;           // LED flash min. brightness (0-255)
+  int flashBrightnessMax =  255;          // LED flash max. brightness (0-255)
+
+  int flashDurationMin = 1;               // Min. duration of each seperate flash
+  int flashDurationMax = 50;              // Max. duration of each seperate flash
+
+  int nextFlashDelayMin = 1;              // Min, delay between each flash and the next
+  int nextFlashDelayMax = 150;            // Max, delay between each flash and the next
+
+  int thunderDelay = random (500, 3000);  // Min. and max. delay between flashing and playing sound
+  int thunderFile = random (1, 17);       // There are 17 soundfiles: 0001.mp3 ... 0017.mp3
+  int loopDelay = random (5000, 30000);   // Min. and max. delay between each loop
+
+  Serial.println();
+  Serial.print(F("Flashing, count: "));
+  Serial.println( flashCount );
+
+  for (int flash = 0 ; flash <= flashCount; flash += 1) { // Flashing LED strip in a loop, random count
+
+    analogWrite(ledPin, random (flashBrightnessMin, flashBrightnessMax)); // Turn LED strip on, random brightness
+    delay(random(flashDurationMin, flashDurationMax)); // Keep it tured on, random duration
+
+    analogWrite(ledPin, 0); // Turn the LED strip off
+    delay(random(nextFlashDelayMin, nextFlashDelayMax)); // Random delay before next flash
+  }
+
+  Serial.print(F("Pausing before playing thunder sound, milliseconds: "));
+  Serial.println(thunderDelay);
+  delay(thunderDelay);
+
+  Serial.print(F("Playing thunder sound, file number: "));
+  Serial.println(thunderFile);
+  myDFPlayer.playMp3Folder(thunderFile);
+  delay(1000); // Give the DFPlayer some time
+
+  while (digitalRead(busyPin) == LOW) { // Wait for the DFPlayer to finish playing the MP3 file
+  }
+
+  Serial.print(F("Pausing before next loop, milliseconds: "));
+  Serial.println(loopDelay);
+  delay(loopDelay);
+
+}
