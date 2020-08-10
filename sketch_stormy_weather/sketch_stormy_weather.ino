cbe8423d021d319853f4e4ff3688c9cc7362b1b3
bhosley <brandonscotthosley@gmail.com>
2020-08-10 11:34:15 -0600

    Update sketch_stormy_weather.ino
    Change Pin for testing
    Change Brightness definition to ternary
    Add strip show to correct lights not turning off

--- sketch_stormy_weather/sketch_stormy_weather.ino
+++ sketch_stormy_weather/sketch_stormy_weather.ino
@@ -3,7 +3,7 @@
 #include <Adafruit_NeoPixel.h>
 
 #define NUM_LEDS 4
-#define LED_PIN 4
+#define LED_PIN 2
 
 int currentIndex = 0;
 float y[] = { 0, 7, 10, 9, 7.1, 7.5, 7.4, 12, 15, 10, 0, 3, 3.5, 4, 1, 7, 1 }; // Using values from Molly Nicholas
@@ -35,12 +35,9 @@ void loop()
     if (random(10) == 3) {    // Represents the Inverse probability of lightning strike
       int led = random(NUM_LEDS);
       for (int i = 0; i < 10; i++) {
-        float brightness;
-        if (random(1) == 1) {
-          float brightness = sma.addValue(y[currentIndex]);
-        } else {
-          float brightness = rma.addValue(y[currentIndex]);
-        }
+        float brightness = random(1) == 1 ? 
+          rma.addValue(y[currentIndex]) : sma.addValue(y[currentIndex]);
+
         float scaledWhite = abs(brightness*500);
 
         strip.setPixelColor(random(NUM_LEDS), strip.Color(scaledWhite, scaledWhite, scaledWhite));
@@ -57,6 +54,7 @@ void loop()
       /* Set Strike chance low*/
     }
     strip.clear();
+    strip.show();
     delay(1000); /* Time between potential storm sequences; add read from potentiometer? */
   } // End Storm Sequence
 }
