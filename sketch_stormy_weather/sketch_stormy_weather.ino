d6e2a29d90ea7f4dbf6710daba0661f66e599b87
bhosley <brandonscotthosley@gmail.com>
2020-08-11 00:44:29 -0600

    Update sketch_stormy_weather.ino
    Hardware testing functions

--- sketch_stormy_weather/sketch_stormy_weather.ino
+++ sketch_stormy_weather/sketch_stormy_weather.ino
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #include "MovingAverage.h"
-#include <Adafruit_NeoPixel.h>
+#include "Adafruit_NeoPixel.h"
 
 #define NUM_LEDS 4
 #define LED_PIN 2
 
+int invStrikeChance = 10;
 int currentIndex = 0;
 float y[] = { 0, 7, 10, 9, 7.1, 7.5, 7.4, 12, 15, 10, 0, 3, 3.5, 4, 1, 7, 1 }; // Using values from Molly Nicholas
 int lenY = sizeof(y) / sizeof(y[0]);
@@ -20,9 +21,10 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ
     //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
     //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
     //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-
+    
 void setup()
 {
+  pinMode(1, OUTPUT); //LED on Model A  
   pinMode(LED_PIN, OUTPUT); // Necessary call for Attiny85
   
   // Neopixel setup
@@ -34,7 +36,7 @@ void loop()
 {
   // Begin Ambient Noise
   if (true) {    // Thunder&Lightning Switch (Storm Scene Sequence)
-    if (random(10) == 3) {    // Represents the Inverse probability of lightning strike
+    if (random(invStrikeChance) == 3) {    // Represents the Inverse probability of lightning strike
       int led = random(NUM_LEDS);
       for (int i = 0; i < 10; i++) {
         float brightness = random(1) == 1 ? 
@@ -48,12 +50,17 @@ void loop()
 
         currentIndex++;
         currentIndex = currentIndex % lenY;
-        
+        /*------------- Test ------------ */
+        digitalWrite(1, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(1000);               // wait for a second
+        digitalWrite(1, LOW);     // turn the LED off by making the voltage LOW
+        delay(1000);               // wait for a second
+        /*------------- Test ------------ */
         /* call for thunder clap */
       }
-      /* Set Strike chance high for increased odds of immediate follow-up */
+      invStrikeChance = 5;
     } else {
-      /* Set Strike chance low*/
+      invStrikeChance = 10;
     }
     for (int i = 0; i < NUM_LEDS; i++) {
       strip.setPixelColor(i, 0);
